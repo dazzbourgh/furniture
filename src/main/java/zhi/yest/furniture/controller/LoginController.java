8fbfa309c727cb1e653a3439a233033e1498817b
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2017-11-24 22:16:50 +0300

    fixed everything

--- src/main/java/zhi/yest/furniture/controller/LoginController.java
+++ src/main/java/zhi/yest/furniture/controller/LoginController.java
@@ -4,18 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("login")
+@Controller
+@RequestMapping("/login")
 public class LoginController {
     @GetMapping
-    public String login() {
+    public String login(Model model, Boolean error) {
+        model.addAttribute("error", error);
         return "login";
     }
 
     @PostMapping
     public String postLogin() {
         // TODO Enable form login with Spring Security (trigger error for now)
-        return "pages/welcome";
+        return "redirect:/welcome";
     }
 
     @GetMapping("/login-error")
